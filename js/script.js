f62cb7f68ed9bba242cf0550604af64fa76ecaaa
Maria <94925262+Maria-developer@users.noreply.github.com>
2022-02-05 20:46:57 -0500

    Validate and log player's guesses

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(".guessed-letter");
+const guessedLettersElement = document.querySelector(".guessed-letter");
 const guessButton = document.querySelector(".guess");
 const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
@@ -7,23 +7,118 @@ const remainingGuessesSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 const word = "magnolia";
+const guessedLetters = [];
 
-// Display dots as placeholders for each letter of the word.
+
+// --------------- Display dots as placeholders for each letter of the word ---------------
 
 function wordPlaceholder(word) {
+
+    // Use an empty array to add items
+    // Each letter of the word will become an item in this array
     const letterPlaceholders = [];
+
+    // Loop over the letters in the array
     for (const letter of word) {
+
+        // Log out each letter to the console
         console.log(letter);
+        // Example (in console):
+        // m
+        // a
+        // g
+        // n
+        // o
+        // l
+        // i
+        // a
+
+        // Make the value of each item display on the page as a dot instead of a letter
         letterPlaceholders.push("●");
+        // Example: ●, ●, ●, ●, ●, ●, ●, ●
     }
+
+    // Joins placeholder dots side by side, so they aren't separated by commas
     wordInProgress.innerText = letterPlaceholders.join("");
+    // Example: ● ● ● ● ● ● ● ●
 };
 
+// Call this function and pass it the word variable as an argument
+// Displays word placeholder with enough dots to represent each letter
 wordPlaceholder(word);
 
+
+// --------------- When Guess button is clicked ---------------
+
 guessButton.addEventListener("click", function (e) {
+
+    // Prevent default form behaviour (submitting form and reloading page) when button is clicked
     e.preventDefault();
-    let guess = letterInput.value;
-    console.log(guess);
+
+    // Empty message
+    message.innerText = "";
+
+    // Grab input entered by player
+    const guess = letterInput.value;
+
+    // Confirm input is valid (input must be a single letter, input must not be empty)
+    // If invalid, a message will appear based on the validate() function's conditions below
+    const validGuess = validate(guess);
+
+    // If valid, log player's guess
+    if (validGuess) {
+        makeGuess(guess);
+    }
+
+    // Empty input value, so the next guess can be entered
     letterInput.value = "";
 });
+
+
+// --------------- Validate player's input ---------------
+
+function validate(input) {
+
+    // An acceptable input is a letter from A to Z
+    const acceptedLetter = /[a-zA-Z]/;
+
+    // If input is empty, instruct player to enter a letter
+    if (input.length === 0) {
+        message.innerText = "Please enter a letter.";
+    }
+
+    // If input is more than one letter, instruct player to enter a single letter
+    else if (input.length > 1) {
+        message.innerText = "Please enter a single letter."
+    }
+
+    // If input doesn't match any accepted values, instruct player to enter a letter from A to Z
+    else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please enter a letter from A to Z."
+    }
+
+    // If none of the above conditions are met, return the inputted letter
+    else {
+        return input;
+    }
+};
+
+
+// --------------- Log player's input ---------------
+
+function makeGuess(guess) {
+
+    // Convert all guesses to uppercase to avoid case-sensitivity issues
+    guess = guess.toUpperCase();
+
+    // If player guesses the same letter again, notify them
+    if (guessedLetters.includes(guess)) {
+        message.innerText = "You already guessed that letter. Please try again."
+    }
+
+    // If player guesses a letter they haven't already guessed, add it to guessedLetters array
+    else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+};
