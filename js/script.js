8e1901c988ac494773b78346707a1fb1c79cbc83
Maria <94925262+Maria-developer@users.noreply.github.com>
2022-02-07 12:41:42 -0500

    Display guessed letters, update word in progress, and check if player won

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLettersElement = document.querySelector(".guessed-letter");
+const guessedLettersList = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
@@ -16,14 +16,14 @@ function wordPlaceholder(word) {
 
     // Use an empty array to add items
     // Each letter of the word will become an item in this array
-    const letterPlaceholders = [];
+    const letterPlaceholder = [];
 
-    // Loop over the letters in the array
+    // Loop over each letter in the array one at a time
     for (const letter of word) {
 
-        // Log out each letter to the console
+        // For every loop of a letter, log that letter in the console
         console.log(letter);
-        // Example (in console):
+        // Example output (in console):
         // m
         // a
         // g
@@ -33,18 +33,20 @@ function wordPlaceholder(word) {
         // i
         // a
 
-        // Make the value of each item display on the page as a dot instead of a letter
-        letterPlaceholders.push("●");
-        // Example: ●, ●, ●, ●, ●, ●, ●, ●
+        // Each time a letter is logged, add a dot to the end of the array
+        letterPlaceholder.push("●");
+        // For example, when "m" is looped, "m" is logged in the console and a dot is pushed to the array
+        // When "a" is looped, "a" is logged and another dot is added to the end
+        // This continues until every letter in the array is looped over, pushing as many dots as there are letters to the array
+        // Example output (on webpage): ●, ●, ●, ●, ●, ●, ●, ●
     }
 
-    // Joins placeholder dots side by side, so they aren't separated by commas
-    wordInProgress.innerText = letterPlaceholders.join("");
-    // Example: ● ● ● ● ● ● ● ●
+    // Join placeholder dots side by side, so they aren't separated by commas
+    wordInProgress.innerText = letterPlaceholder.join("");
+    // Example output (on webpage): ● ● ● ● ● ● ● ●
 };
 
-// Call this function and pass it the word variable as an argument
-// Displays word placeholder with enough dots to represent each letter
+// Call function with word argument to display placeholders for each letter
 wordPlaceholder(word);
 
 
@@ -62,7 +64,7 @@ guessButton.addEventListener("click", function (e) {
     const guess = letterInput.value;
 
     // Confirm input is valid (input must be a single letter, input must not be empty)
-    // If invalid, a message will appear based on the validate() function's conditions below
+    // If invalid, a message will appear based on the validate() function's conditions
     const validGuess = validate(guess);
 
     // If valid, log player's guess
@@ -97,7 +99,7 @@ function validate(input) {
         message.innerText = "Please enter a letter from A to Z."
     }
 
-    // If none of the above conditions are met, return the inputted letter
+    // If none of the above conditions are met, return the submitted letter
     else {
         return input;
     }
@@ -116,9 +118,81 @@ function makeGuess(guess) {
         message.innerText = "You already guessed that letter. Please try again."
     }
 
-    // If player guesses a letter they haven't already guessed, add it to guessedLetters array
+    // If player guesses a letter they haven't already guessed, add it to the guessedLetters array
     else {
         guessedLetters.push(guess);
         console.log(guessedLetters);
+
+        // Display guessed letters list on page
+        showGuessedLetters();
+
+        // Update word in progress based on player's guesses
+        updateWordInProgress(guessedLetters);
+    }
+};
+
+
+// --------------- Show guessed letters ---------------
+
+function showGuessedLetters() {
+
+    // First, clear guessed letters list
+    guessedLettersList.innerHTML = "";
+
+    // Make each guessed letter a list item
+    for (const letter of guessedLetters) {
+        const li = document.createElement("li");
+        li.innerText = letter;
+
+        // Add each letter to the guessed letters list
+        guessedLettersList.append(li);
+    }
+};
+
+
+// --------------- Update word in progress based on player's guesses ---------------
+
+function updateWordInProgress(guessedLetters) {
+
+    // Change word variable to uppercase
+    const wordUpper = word.toUpperCase();
+
+    // Split word string into an array of letters
+    const wordArray = wordUpper.split("");
+
+    // Add letters to a new array
+    const revealWord = [];
+
+    // Loop over each letter
+    for (const letter of wordArray) {
+
+        // If the guessedLetters array contains the letter being looped, replace the dot placeholder with the correct letter
+        if (guessedLetters.includes(letter)) {
+            revealWord.push(letter);
+        } 
+
+        // If the guessedLetters array does not include the looped letter, continue to display a dot placeholder at that index
+        else {
+            revealWord.push("●");
+        }
+
+    // Join correct letters and placeholders into a string to show updated word in progress
+    wordInProgress.innerText = revealWord.join("");
+
+    // Check if player won
+    checkIfWin();
+    }
+};
+
+// --------------- Check if player won ---------------
+
+function checkIfWin () {
+
+    // Check if the word in uppercase (to avoid case-sinsitivity issues) matches wordInProgress (already uppercase)
+    if (word.toUpperCase() === wordInProgress.innerText) {
+
+        // If so, add win class and display a message letting the player know they won
+        message.classList.add("win");
+        message.innerHTML = `<p class="highlight">You guessed the word! Congrats!</p>`;
     }
 };
